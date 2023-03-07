4a3b05c11ba90765722e7bfb0070ef443dabb3d1
dyoung2012 <108146998+dyoung2012@users.noreply.github.com>
2023-03-07 14:13:45 -0800

    added index.html

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+def other():
+    return render_template('contact.html')
+def other():
+    return render_template('about.html')
 
 
 
